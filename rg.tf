31fd1a9e27ee3026d00286e464772cbd5fca2c0f
PAGUTLA CHIRANJEEVI <you@example.com>
2024-12-05 22:30:15 +0530

    added rg2 file

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "jeeni"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example" {
+  name     = "dere"
+  location = "West Europe"
+}
