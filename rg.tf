4f3494287b1ff5bcb6281a15306790320b32605c
PAGUTLA CHIRANJEEVI <you@example.com>
2024-12-05 22:35:19 +0530

    added

--- rg.tf
+++ rg.tf
@@ -1,6 +1,9 @@
 resource "azurerm_resource_group" "example" {
   name     = "jeeni"
   location = "West Europe"
+  tags = {
+    owner = "IT"
+  }
 }
 resource "azurerm_resource_group" "example" {
   name     = "dere"
